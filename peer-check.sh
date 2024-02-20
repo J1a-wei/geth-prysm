a904c81233385a3186e907f894522d8605280b5f
skrbug <skrbug@outlook.com>
2024-02-20 11:48:49 +0800

    inbound p2p

--- peer-check.sh
+++ peer-check.sh
@@ -2,4 +2,4 @@
 curl localhost:8545 -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}'
 curl localhost:8545 -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"admin_peers","params":[],"id":1}'
 
-curl localhost:8080/eth/v1/node/peers?state=connectedg -H 'Content-Type: application/json' 
+curl "localhost:8080/eth/v1/node/peers?state=connected&direction=inbound" -H 'Content-Type: application/json' 
